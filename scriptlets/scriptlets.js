5a9b48c8edb791489d50d4a23a56177d8a8dfb00
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2022-02-04 15:13:08 -0800

    Update scriptlets.js

--- scriptlets/scriptlets.js
+++ scriptlets/scriptlets.js
@@ -3,45 +3,51 @@
 /// apple-music-album-artwork-resizer-and-english-translation.js
 /// alias amaaraet.js
 // Based on: https://github.com/MrBukLau/userscripts/blob/master/javascripts/apple-music-album-artwork-resizer-and-english-translation.user.js
-if (document.location.href.includes("music.apple.com")) {
-    var oldUrlSearch = window.location.search;
-    if (window.location.href.indexOf("?i=") > -1) {
-        if (!/\&l=en$/.test(oldUrlSearch)) {
-            var newURL = window.location.protocol + "//" + window.location.host + window.location.pathname + oldUrlSearch + "&l=en" + window.location.hash;
-            window.location.replace(newURL);
+(function() {
+    if (document.location.href.includes("music.apple.com")) {
+        var oldUrlSearch = window.location.search;
+        if (window.location.href.indexOf("?i=") > -1) {
+            if (!/\&l=en$/.test(oldUrlSearch)) {
+                var newURL = window.location.protocol + "//" + window.location.host + window.location.pathname + oldUrlSearch + "&l=en" + window.location.hash;
+                window.location.replace(newURL);
+            }
+        } else {
+            if (!/\?l=en$/.test(oldUrlSearch)) {
+                var newURL = window.location.protocol + "//" + window.location.host + window.location.pathname + oldUrlSearch + "?l=en" + window.location.hash;
+                window.location.replace(newURL);
+            }
         }
-    } else {
-        if (!/\?l=en$/.test(oldUrlSearch)) {
-            var newURL = window.location.protocol + "//" + window.location.host + window.location.pathname + oldUrlSearch + "?l=en" + window.location.hash;
-            window.location.replace(newURL);
+    } else if (document.location.href.includes("mzstatic.com/image/thumb")) {
+        if (location.href.match("600x600bb.webp")) {
+            location.href = location.href.replace("600x600bb.webp", "2000x2000.jpeg");
+        } else if (location.href.match("600x600bf.webp")) {
+            location.href = location.href.replace("600x600bf.webp", "2000x2000.jpeg");
+        } else if (location.href.match("1000x1000bb.webp")) {
+            location.href = location.href.replace("1000x1000bb.webp", "2000x2000.jpeg");
         }
     }
-} else if (document.location.href.includes("mzstatic.com/image/thumb")) {
-    if (location.href.match("600x600bb.webp")) {
-        location.href = location.href.replace("600x600bb.webp", "2000x2000.jpeg");
-    } else if (location.href.match("600x600bf.webp")) {
-        location.href = location.href.replace("600x600bf.webp", "2000x2000.jpeg");
-    } else if (location.href.match("1000x1000bb.webp")) {
-        location.href = location.href.replace("1000x1000bb.webp", "2000x2000.jpeg");
-    }
-}
+})();
 
 /// hikarinoakariost-bypasser.js
 /// alias hnab.js
 // Based on: https://stackoverflow.com/questions/17883692/how-to-set-time-delay-in-javascript
-if (document.location.href.includes("hikarinoakari.com/out")) {
-    var delayInMilliseconds = 1000;
-    setTimeout(function() {
-        document.querySelector("a[class='link']").click();
-    }, delayInMilliseconds);
-}
+(function() {
+    if (document.location.href.includes("hikarinoakari.com/out")) {
+        var delayInMilliseconds = 1000;
+        setTimeout(function() {
+            document.querySelector("a[class='link']").click();
+        }, delayInMilliseconds);
+    }
+})();
 
 /// old-reddit-redirection.js
 /// alias orr.js
 // Based on: https://greasyfork.org/en/scripts/44669-bring-back-old-reddit
-if (document.location.href.includes("www.reddit.com") && !document.location.href.includes("www.reddit.com/poll")) {
-    window.location.replace("https://old.reddit.com" + window.location.pathname + window.location.search);
-}
+(function() {
+    if (document.location.href.includes("www.reddit.com") && !document.location.href.includes("www.reddit.com/poll")) {
+        window.location.replace("https://old.reddit.com" + window.location.pathname + window.location.search);
+    }
+})();
 
 /// tinyurl-is-bypasser.js
 /// alias tuib.js
@@ -55,4 +61,4 @@ if (document.location.href.includes("www.reddit.com") && !document.location.href
             }
         }
     })
-})()
+})();
