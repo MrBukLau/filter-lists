2e72dfee5301d983c95d510e584d111fba752e2d
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2022-02-09 16:42:30 -0800

    Update scriptlets.js

--- scriptlets/scriptlets.js
+++ scriptlets/scriptlets.js
@@ -1,8 +1,24 @@
 // MrBukLau's Scriptlets
 
-/// apple-music-album-artwork-resizer-and-english-translation.js
-/// alias amaaraet.js
-// Based on: https://github.com/MrBukLau/userscripts/blob/master/javascripts/apple-music-album-artwork-resizer-and-english-translation.user.js
+/// apple-music-album-artwork-resizer.js
+/// alias amaar.js
+// Based on: https://greasyfork.org/en/discussions/requests/55817-replace-string-in-an-url
+(function() {
+    'use strict';
+    if (document.location.href.includes("mzstatic.com/image/thumb")) {
+        if (location.href.match("600x600bb.webp")) {
+            location.href = location.href.replace("600x600bb.webp", "2000x2000.jpeg");
+        } else if (location.href.match("600x600bf.webp")) {
+            location.href = location.href.replace("600x600bf.webp", "2000x2000.jpeg");
+        } else if (location.href.match("1000x1000bb.webp")) {
+            location.href = location.href.replace("1000x1000bb.webp", "2000x2000.jpeg");
+        }
+    }
+})();
+
+/// apple-music-english-translation.js
+/// alias amet.js
+// Based on: https://stackoverflow.com/questions/16065937/changing-a-pages-url-parameters
 (function() {
     'use strict';
     if (document.location.href.includes("music.apple.com")) {
@@ -17,14 +33,6 @@
                 var newURL = window.location.protocol + "//" + window.location.host + window.location.pathname + oldUrlSearch + "?l=en" + window.location.hash;
                 window.location.replace(newURL);
             }
-        }
-    } else if (document.location.href.includes("mzstatic.com/image/thumb")) {
-        if (location.href.match("600x600bb.webp")) {
-            location.href = location.href.replace("600x600bb.webp", "2000x2000.jpeg");
-        } else if (location.href.match("600x600bf.webp")) {
-            location.href = location.href.replace("600x600bf.webp", "2000x2000.jpeg");
-        } else if (location.href.match("1000x1000bb.webp")) {
-            location.href = location.href.replace("1000x1000bb.webp", "2000x2000.jpeg");
         }
     }
 })();
