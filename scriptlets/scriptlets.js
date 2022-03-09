917ad3ac78ecec0f832311332617abbaed81a118
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2022-03-09 11:50:08 -0800

    Update scriptlets.js

--- scriptlets/scriptlets.js
+++ scriptlets/scriptlets.js
@@ -4,9 +4,9 @@
 /// alias amafc.js
 (function() {
     "use strict";
-    if (document.location.href.includes(".mzstatic.com/image/thumb/")) {
-        if (location.href.match(".webp")) {
-            location.href = location.href.replace(".webp", ".jpeg");
+    if (window.location.href.includes(".mzstatic.com/image/thumb/")) {
+        if (window.location.href.match(".webp")) {
+            window.location.href = window.location.href.replace(".webp", ".jpeg");
         }
     }
 })();
@@ -15,31 +15,31 @@
 /// alias amasc.js
 (function() {
     "use strict";
-    if (document.location.href.includes(".mzstatic.com/image/thumb/")) {
-        if (location.href.match("190x190")) {
-            location.href = location.href.replace("190x190", "2000x2000");
-        } else if (location.href.match("200x200")) {
-            location.href = location.href.replace("200x200", "2000x2000");
-        } else if (location.href.match("270x270")) {
-            location.href = location.href.replace("270x270", "2000x2000");
-        } else if (location.href.match("300x300")) {
-            location.href = location.href.replace("300x300", "2000x2000");
-        } else if (location.href.match("305x305")) {
-            location.href = location.href.replace("305x305", "2000x2000");
-        } else if (location.href.match("380x380")) {
-            location.href = location.href.replace("380x380", "2000x2000");
-        } else if (location.href.match("400x400")) {
-            location.href = location.href.replace("400x400", "2000x2000");
-        } else if (location.href.match("500x500")) {
-            location.href = location.href.replace("500x500", "2000x2000");
-        } else if (location.href.match("540x540")) {
-            location.href = location.href.replace("540x540", "2000x2000");
-        } else if (location.href.match("600x600")) {
-            location.href = location.href.replace("600x600", "2000x2000");
-        } else if (location.href.match("610x610")) {
-            location.href = location.href.replace("610x610", "2000x2000");
-        } else if (location.href.match("1000x1000")) {
-            location.href = location.href.replace("1000x1000", "2000x2000");
+    if (window.location.href.includes(".mzstatic.com/image/thumb/")) {
+        if (window.location.href.match("190x190")) {
+            window.location.href = window.location.href.replace("190x190", "2000x2000");
+        } else if (window.location.href.match("200x200")) {
+            window.location.href = window.location.href.replace("200x200", "2000x2000");
+        } else if (window.location.href.match("270x270")) {
+            window.location.href = window.location.href.replace("270x270", "2000x2000");
+        } else if (window.location.href.match("300x300")) {
+            window.location.href = window.location.href.replace("300x300", "2000x2000");
+        } else if (window.location.href.match("305x305")) {
+            window.location.href = window.location.href.replace("305x305", "2000x2000");
+        } else if (window.location.href.match("380x380")) {
+            window.location.href = window.location.href.replace("380x380", "2000x2000");
+        } else if (window.location.href.match("400x400")) {
+            window.location.href = window.location.href.replace("400x400", "2000x2000");
+        } else if (window.location.href.match("500x500")) {
+            window.location.href = window.location.href.replace("500x500", "2000x2000");
+        } else if (window.location.href.match("540x540")) {
+            window.location.href = window.location.href.replace("540x540", "2000x2000");
+        } else if (window.location.href.match("600x600")) {
+            window.location.href = window.location.href.replace("600x600", "2000x2000");
+        } else if (window.location.href.match("610x610")) {
+            window.location.href = window.location.href.replace("610x610", "2000x2000");
+        } else if (window.location.href.match("1000x1000")) {
+            window.location.href = window.location.href.replace("1000x1000", "2000x2000");
         }
     }
 })();
@@ -48,7 +48,7 @@
 /// alias amete.js
 (function() {
     "use strict";
-    if (document.location.href.includes("/music.apple.com/")) {
+    if (window.location.href.includes("/music.apple.com/")) {
         let oldUrlSearch = window.location.search;
         if ((oldUrlSearch.indexOf("?") > -1) && (oldUrlSearch.indexOf("?l=") === -1)) {
             if (!/\&l=en$/.test(oldUrlSearch)) {
@@ -68,7 +68,7 @@
 /// alias hnab.js
 (function() {
     "use strict";
-    if (document.location.href.includes("/hikarinoakari.com/out/")) {
+    if (window.location.href.includes("/hikarinoakari.com/out/")) {
         let delayInMilliseconds = 1000;
         setTimeout(function() {
             document.querySelector("a[class='link']").click();
@@ -81,7 +81,7 @@
 (function() {
     "use strict";
     window.addEventListener("load", function() {
-        if (document.location.href.includes("/itdmusic.top/")) {
+        if (window.location.href.includes("/itdmusic.top/")) {
             let firstButton = document.getElementsByClassName("btn btn-primary btn-captcha");
             if (firstButton.length > 0) {
                 document.querySelector("button[class='btn btn-primary btn-captcha']").click();
@@ -105,7 +105,7 @@
 (function() {
     "use strict";
     window.addEventListener("load", function() {
-        if (document.location.href.includes("/old.reddit.com/")) {
+        if (window.location.href.includes("/old.reddit.com/")) {
             let aCol = document.getElementsByTagName("a");
             let a, actualUrl;
             for (let i = 0; i < aCol.length; i++) {
@@ -123,8 +123,10 @@
 /// alias orre.js
 (function() {
     "use strict";
-    if (document.location.href.includes("/www.reddit.com/") && !document.location.href.includes("/www.reddit.com/poll/")) {
-        window.location.replace("https://old.reddit.com" + window.location.pathname + window.location.search);
+    if (window.location.href.includes("/www.reddit.com/") && !window.location.href.includes("/www.reddit.com/poll/")) {
+        if (window.location.href.match("/www.reddit.com/")) {
+            window.location.href = window.location.href.replace("/www.reddit.com/", "/old.reddit.com/");
+        }
     }
 })();
 
@@ -133,7 +135,7 @@
 (function() {
     "use strict";
     window.addEventListener("load", function() {
-        if (document.location.href.includes("/ouo.io/") || document.location.href.includes("/ouo.press/")) {
+        if (window.location.href.includes("/ouo.io/") || window.location.href.includes("/ouo.press/")) {
             if (document.getElementById("form-captcha") === null) {
                 document.getElementsByTagName("form")[0].submit();
             }
@@ -149,7 +151,7 @@
 (function() {
     "use strict";
     window.addEventListener("load", function() {
-        if (document.location.href.includes("/rentry.co/") || document.location.href.includes("/rentry.org/")) {
+        if (window.location.href.includes("/rentry.co/") || window.location.href.includes("/rentry.org/")) {
             let anchor = document.getElementsByTagName("a");
             for (let i = 0; i < anchor.length; i++) {
                 anchor[i].setAttribute("target", "_blank");
@@ -163,7 +165,7 @@
 (function() {
     "use strict";
     window.addEventListener("load", function() {
-        if (document.location.href.includes("/tinyurl.is/")) {
+        if (window.location.href.includes("/tinyurl.is/")) {
             let targetLink = $("a:contains('Click Here to Watch')");
             if (targetLink && targetLink.length) {
                 window.location.href = targetLink[0].href;
