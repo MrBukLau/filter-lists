81ba6a556591caaa8c20ac7331a808b43e0d7b59
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2022-03-11 16:28:42 -0800

    Update scriptlets.js

--- scriptlets/scriptlets.js
+++ scriptlets/scriptlets.js
@@ -1,5 +1,98 @@
 // MrBukLau's Scriptlets
 
+// Generic Scriptlets
+
+/// set-attr.js
+/// alias sa.js
+(function() {
+    "use strict";
+    const token = '{{1}}';
+    if (token === '' || token === '{{1}}') {
+        return;
+    }
+    const tokens = token.split(/\s*\|\s*/);
+    const attrValue = '{{2}}';
+    let selector = '{{3}}';
+    if (selector === '' || selector === '{{3}}') {
+        selector = `[${tokens.join('],[')}]`;
+    }
+    let timer;
+    const behavior = '{{4}}';
+    const setattr = () => {
+        timer = undefined;
+        const nodes = document.querySelectorAll(selector);
+        try {
+            for (const node of nodes) {
+                for (const attr of tokens) {
+                    if (attr !== attrValue) {
+                        node.setAttribute(attr, attrValue);
+                    }
+                }
+            }
+        } catch {}
+    };
+    const mutationHandler = mutations => {
+        if (timer !== undefined) {
+            return;
+        }
+        let skip = true;
+        for (let i = 0; i < mutations.length && skip; i++) {
+            const {
+                type,
+                addedNodes,
+                removedNodes
+            } = mutations[i];
+            if (type === 'attributes') {
+                skip = false;
+            }
+            for (let j = 0; j < addedNodes.length && skip; j++) {
+                if (addedNodes[j].nodeType === 1) {
+                    skip = false;
+                    break;
+                }
+            }
+            for (let j = 0; j < removedNodes.length && skip; j++) {
+                if (removedNodes[j].nodeType === 1) {
+                    skip = false;
+                    break;
+                }
+            }
+        }
+        if (skip) {
+            return;
+        }
+        timer = self.requestIdleCallback(setattr, {
+            timeout: 10
+        });
+    };
+    const start = () => {
+        setattr();
+        if (/\bloop\b/.test(behavior) === false) {
+            return;
+        }
+        const observer = new MutationObserver(mutationHandler);
+        observer.observe(document.documentElement, {
+            attributes: true,
+            attributeFilter: tokens,
+            childList: true,
+            subtree: true,
+        });
+    };
+    if (document.readyState !== 'complete' && /\bcomplete\b/.test(behavior)) {
+        window.addEventListener('load', start, {
+            once: true
+        });
+    } else if (document.readyState === 'loading') {
+        window.addEventListener('DOMContentLoaded', start, {
+            once: true
+        });
+    } else {
+        start();
+    }
+})();
+
+// Specific Scriptlets
+
 /// apple-music-artwork-format-and-size-changer.js
 /// alias amafasc.js
 (function() {
